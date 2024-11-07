516d1df7a8e1a8e4b25ea165fbb757b5c4214126
Nicolas Mollo <nicolas.mollo.alanguia@cftmail.cl>
2024-11-07 11:25:26 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { UbicanosComponent } from './componentes/ubicanos/ubicanos.component';
+import { ContactanosComponent } from './componentes/contactanos/contactanos.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    UbicanosComponent,
+    ContactanosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
